27993d70d12a61cdb045dce622f3ab344d2a89b2
Youen FROGER <youen.froger@etu.emse.fr>
2018-04-18 15:36:00 +0200

    website prints helloworld, and logs in the console each time someone connects to the server on port 8080

---
+++ LogModule/logger.js
@@ -0,0 +1,7 @@
+// this was developped in order to translate later the log to a textfile or whatever
+function log(message)
+{
+	console.log(message);
+}
+
+module.exports.log = log;
--- exemple/bonjour.js
+++ exemple/bonjour.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+// website for helloworld
+// ** includes ** //
+var http = require("http");
+var logger = require("./../LogModule/logger.js");
+var server = http.createServer();
+server.on("request",
+	(request,response) => 
+	{
+		logger.log("connection recieved");
+		response.writeHead(200, 
+			{
+				"Content-type" : "text/html; charset = utf-8"
+		
+
+
+			})
+		response.end("hello world");
+	});
+server.listen(8080);
